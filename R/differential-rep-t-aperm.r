8028bb52931d01ad2e25997183174a60c594e0fd
HervePerdry <herve.perdry@gmail.com>
2024-02-28 16:11:26 +0100

    propagated some changes / added some forgotten names(.)<-

--- R/differential-rep-t-aperm.r
+++ R/differential-rep-t-aperm.r
@@ -4,6 +4,7 @@ rep.differential <- function(x, ...) {
   x <- unclass(x)
   L <- vector("list", length(x))
   for(i in seq_along(x)) L[[i]] <- rep(x[[i]], ...)
+  names(L) <- names(x)
   class(L) <- "differential"
   L
 }
@@ -15,6 +16,7 @@ t.differential <- function(x) {
   x <- unclass(x)
   L <- vector("list", length(x))
   for(i in seq_along(x)) L[[i]] <- t(x[[i]])
+  names(L) <- names(x)
   class(L) <- "differential"
   L
 }
@@ -25,6 +27,7 @@ aperm.differential <- function(a, perm = NULL, resize = TRUE, ...) {
   a <- unclass(a)
   x <- vector("list", length(a))
   for(i in seq_along(a)) x[[i]] <- aperm(a[[i]], perm, resize, ...)
+  names(x) <- names(a)
   class(x) <- "differential"
   x
 }
