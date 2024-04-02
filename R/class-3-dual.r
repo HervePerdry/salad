bd4891552ae3b30ecbb1d60b443b814c8f4138db
HervePerdry <herve.perdry@gmail.com>
2024-04-02 17:27:56 +0200

    fignolages dans dual() [varnames] et dans méthode "show"

--- R/class-3-dual.r
+++ R/class-3-dual.r
@@ -7,6 +7,21 @@ setClass("dual", slots = c(x = "numericOrArray", d = "differential"))
 setMethod("show", "dual",
     function(object) {
       print(object@x)
-      cat("[has derivatives]\n")
+      wd <- options("width")$width
+      s <- "[has derivatives]\n"
+      if(wd > 20) { 
+        nn <- varnames(object);
+        s1 <- paste0("[has derivatives in ", paste(nn, collapse = " "), "]\n")
+        if(nchar(s1) <= wd) {
+          s <- s1
+        } else {
+          le <- cumsum(1 + nchar(nn))
+          w <- which(le < wd - 24)
+          if(length(w) > 0) {
+            s <- paste0("[has derivatives in ", paste(nn[1:max(w)], collapse = " "), " ...]\n")
+          }
+        }
+      }
+      cat(s)
     })
 
--- R/dual.r
+++ R/dual.r
@@ -7,9 +7,28 @@ dual <- function(x, varnames, dx, constant = FALSE) {
     dx <- lapply(1:n.var, \(i) { v <- rep(0,n.var); v[i] <- 1; dim(v) <- dims; v })
     if(missing(varnames)) {
       if(is.null(dims)) {
-        varnames <- sprintf("x%d", 1:n.var)
+        # vector
+        nn <- names(x)
+        if(is.null(nn)) {
+          varnames <- sprintf("x%d", 1:n.var)
+        } else {
+          varnames <- nn
+        }
+        if(length(varnames) != n.var | anyDuplicated(varnames) | "" %in% varnames) 
+          varnames <- sprintf("x%d", 1:n.var)
       } else {
-        varnames <- paste0("x", dimnamer(dims))
+        # matrix
+        nn <- dimnames(x)
+        if(is.null(nn)) {
+          varnames <- paste0("x", dimnamer(dims))
+        } else {
+          dd <- dim(x)
+          for(i in seq_along(nn)) 
+            if(is.null(nn[[i]])) nn[[i]] <- seq_len(dd[i])
+          varnames <- outer(nn[[1]], nn[[2]], paste, sep = ".")
+        }
+        if(length(varnames) != n.var | anyDuplicated(varnames)) 
+          varnames <- paste0("x", dimnamer(dims))
       }
     } else {
       if(length(varnames) != n.var) stop("varnames and x should have same length")
